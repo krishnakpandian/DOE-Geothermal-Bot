e148bd2462a82f242014c4e50160edcf9f72f575
kpandian <kpandian@ucsc.edu>
2020-12-05 17:44:29 -0800

    updating bot

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const TwitterBot = require('node-twitterbot').TwitterBot;
 var Bot = new TwitterBot(TwitterAuthentication);
 const { facts } = require('./facts');
 
-function messageConcat(factMessage, factNumber){
-    let newMessage = "Fun Fact" + factNumber + ":" + factMessage;
+function messageConcat(factMessage){
+    let newMessage = "Fun Fact: " + factMessage;
     return newMessage;
 }
 
@@ -15,7 +15,26 @@ function getRandomFact(facts) {
 }
 
 var message = getRandomFact(facts);
+var startTime = '15:00:00';
+var endTime = '16:00:00';
 
-Bot.tweet(message, (err) => {
-    console.log(err);
-});
+currentDate = new Date()   
+
+startDate = new Date(currentDate.getTime());
+startDate.setHours(startTime.split(":")[0]);
+startDate.setMinutes(startTime.split(":")[1]);
+startDate.setSeconds(startTime.split(":")[2]);
+
+endDate = new Date(currentDate.getTime());
+endDate.setHours(endTime.split(":")[0]);
+endDate.setMinutes(endTime.split(":")[1]);
+endDate.setSeconds(endTime.split(":")[2]);
+
+
+valid = startDate < currentDate && endDate > currentDate
+
+if (valid) {
+    Bot.tweet(message, (err) => {
+        console.log(err);
+    });
+}
