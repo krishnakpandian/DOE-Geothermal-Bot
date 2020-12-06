a36361653854d5b898cd9920ee41b0e647a40c93
kpandian <kpandian@ucsc.edu>
2020-12-06 12:34:46 -0800

    fixing clock

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ var message = getRandomFact(facts);
 
 
 
-Bot.tweet(message, (err) => {
-    console.log(err);
-});
+currentDate = new Date()   
+
+startDate = new Date(currentDate.getTime());
+startDate.setHours(startTime.split(":")[0]);
+startDate.setMinutes(startTime.split(":")[1]);
+startDate.setSeconds(startTime.split(":")[2]);
+
+endDate = new Date(currentDate.getTime());
+endDate.setHours(endTime.split(":")[0]);
+endDate.setMinutes(endTime.split(":")[1]);
+endDate.setSeconds(endTime.split(":")[2]);
+
+
+valid = startDate < currentDate && endDate > currentDate
+
+if (valid) {
+    Bot.tweet(message, (err) => {
+        console.log(err);
+    });
+} 
