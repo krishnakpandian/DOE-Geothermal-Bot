c82213e7f249b79ffac932e34c52b214b4526385
kpandian <kpandian@ucsc.edu>
2020-12-05 23:33:22 -0800

    update

--- index.js
+++ index.js
@@ -15,26 +15,9 @@ function getRandomFact(facts) {
 }
 
 var message = getRandomFact(facts);
-var startTime = '15:00:00';
-var endTime = '16:00:00';
-
-currentDate = new Date()   
-
-startDate = new Date(currentDate.getTime());
-startDate.setHours(startTime.split(":")[0]);
-startDate.setMinutes(startTime.split(":")[1]);
-startDate.setSeconds(startTime.split(":")[2]);
-
-endDate = new Date(currentDate.getTime());
-endDate.setHours(endTime.split(":")[0]);
-endDate.setMinutes(endTime.split(":")[1]);
-endDate.setSeconds(endTime.split(":")[2]);
 
 
-valid = startDate < currentDate && endDate > currentDate
 
-if (valid) {
-    Bot.tweet(message, (err) => {
-        console.log(err);
-    });
-}
+Bot.tweet(message, (err) => {
+    console.log(err);
+});
