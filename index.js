0f90313c3b7af969295cd0b3f9c35c6d43e30fba
kpandian <kpandian@ucsc.edu>
2020-12-09 00:24:06 -0800

    Adding hashtag

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var Bot = new TwitterBot(TwitterAuthentication);
 const { facts } = require('./facts');
 
 function messageConcat(factMessage){
-    let newMessage = "Fun Fact: " + factMessage;
+    let newMessage = "Fun Fact: " + factMessage + "#Geothermal";
     return newMessage;
 }
 
@@ -18,23 +18,7 @@ var message = getRandomFact(facts);
 
 
 
-currentDate = new Date()   
 
-startDate = new Date(currentDate.getTime());
-startDate.setHours(startTime.split(":")[0]);
-startDate.setMinutes(startTime.split(":")[1]);
-startDate.setSeconds(startTime.split(":")[2]);
-
-endDate = new Date(currentDate.getTime());
-endDate.setHours(endTime.split(":")[0]);
-endDate.setMinutes(endTime.split(":")[1]);
-endDate.setSeconds(endTime.split(":")[2]);
-
-
-valid = startDate < currentDate && endDate > currentDate
-
-if (valid) {
-    Bot.tweet(message, (err) => {
-        console.log(err);
-    });
-} 
+Bot.tweet(message, (err) => {
+    console.log(err);
+});
